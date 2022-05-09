88e0257bedb3c5b46ddc45ea6685743253c34868
AlexFalconCenco <alexnicolas.falcon@externos-cl.cencosud.com>
2022-05-09 17:14:19 -0300

    Kick off

---
+++ app/routes.ts
@@ -0,0 +1,8 @@
+import { Application } from 'express';
+
+//import { getUsers, getUserById, createUser } from './controllers/users';
+
+export const init = (app: Application): void => {
+  //app.get('/users', getUsers);
+  //app.post('/users', createUser);
+};
