594e511660a5e4e224f900971c51d0872d7a9078
AleFalcon <alex251095@hotmail.com>
2022-10-04 17:15:55 -0300

    Modificacion de log

--- app/controllers/user.ts
+++ app/controllers/user.ts
@@ -83,7 +83,7 @@ function uploadFiles(EDFile: UploadedFile) {
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function saveImage(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
   try{
-    console.log(req.files)
+    console.log(req)
     if (req.files != undefined) {
       for(let count = 0; count < 2 ; count++){
         console.log(req.files)
