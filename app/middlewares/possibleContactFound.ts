f2b8dec7a9cbb0efb1415d1bb3a7a0ea62f1d7e3
AleFalcon <alex251095@hotmail.com>
2022-11-16 00:57:35 -0300

    A

--- app/middlewares/possibleContactFound.ts
+++ app/middlewares/possibleContactFound.ts
@@ -23,7 +23,8 @@ export async function contactFound (req: Request, res: Response, next: NextFunct
             } else {
                 const event: Events | undefined = await EventsRepository().findOne({familiar: possibleContacts.familiar.id, carer: possibleContacts.carer as Carers, day: req.query.eventDay as number, status: 0})
                 if(event !== undefined){
-                    let now = new Date();
+                    let now = new Date()
+                    now.setHours(now.getHours()-3)
                     if (event.expirationDate === undefined || now < new Date(event.expirationDate)) {
                         const startDate = new Date()
                         startDate.setHours(Number.parseInt(event.startEvent.split(":")[0])+3, Number.parseInt(event.startEvent.split(":")[1]))
